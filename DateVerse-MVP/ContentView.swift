01d6351c1f8944dc8ad7e50ca22039d8b6e6223b
游哲維 <stevenyu.supreme@gmail.com>
2025-12-17 00:08:14 +0800

    Initial Commit

---
+++ DateVerse-MVP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DateVerse-MVP
+//
+//  Created by 游哲維 on 2025/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DateVerse-MVP/DateVerse_MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateVerse_MVPApp.swift
+//  DateVerse-MVP
+//
+//  Created by 游哲維 on 2025/12/17.
+//
+
+import SwiftUI
+
+@main
+struct DateVerse_MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
